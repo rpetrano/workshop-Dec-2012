9d024a7733687e3c2e42d5808bcd74ff8f5bf3f4
Robert Petranović <robi.petranovic@gmail.com>
2012-12-03 18:55:16 +0000

    Izmjene

--- php/web/app.php
+++ php/web/app.php
@@ -1,20 +1,75 @@
 <?php
-	function generateRandomPerson () {
-        $names = array("joe", "tom", "marko", "robert", "viktor", "jimmy", "rikard", "josipovic", "alan ford", "hrvoje", "ivan");
-        $name = $names[array_rand ($names)];
+require_once __DIR__.'/platform/Bootstrap.php';
 
-        $dan = rand (1, 28);
-        $mjesec = rand (1, 12);
-        $godina = rand (1970, 2000);
+use School\Student;
 
-        $datumRodjenja = $godina . "-" . $mjesec . "-" . $dan;
 
-        $person = new person (array (
-            "ime" => $name,
-            "datumRodjenja" => $datumRodjenja
-        ));
+$marko = new Student(array(
+	'firstName' => 'Tester',
+	'lastName' => 'Bester',
+	'birthdate' => '1980-1-1'
+));
 
-        $person->persist();
+//$marko->persist();
 
-        return $this->redirect ("/person");
-	}	
+
+// ------------------------------------------
+
+function randomWord() {
+	$randomWord = "";
+	$n = rand(3,7);
+	for($i = 0; $i < $n; $i++)
+		$randomWord .= chr(rand(ord('a'), ord('z')));
+	$randomWord = ucfirst($randomWord);
+
+	return $randomWord;
+}
+
+function generateRandomPerson() {
+	$randomDate = new DateTime();
+	$start = new DateTime("1970-1-1");
+	$end = new DateTime("2000-12-31");
+	$randomSecond = rand($start->getTimestamp(), $end->getTimestamp());
+	$randomDate = $randomDate->setTimestamp($randomSecond);
+
+	return new Student(array(
+		"firstName" => randomWord(),
+		'lastName' => randomWord(),
+		"birthdate" => $randomDate
+	));
+}
+
+// ------------------------------------------
+
+//generateRandomPerson()->persist();
+
+
+
+$randomPersons = array();
+for($i = 0; $i < 50; $i++)
+	$randomPersons[] = generateRandomPerson();
+
+
+
+use NGS\Client\StandardProxy;
+$proxy = new StandardProxy();
+//$proxy->insert($randomPersons);
+
+
+
+$allPersons = Student::findAll();
+
+/*
+foreach($allPersons as $person) {
+	echo 'Ja sam ', $person->name, ' i rođen sam ', $person->birthdate, "<br />";
+}
+*/
+foreach (Student::getShortPeople (10) as $person)
+	echo $person->name, "<br />";
+
+use School\Demographic;
+$demo = new Demographic();
+$content = $demo->createPdf();
+
+file_put_contents("minors.pdf", $content);
+
